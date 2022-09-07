169bd95355a3f31c0955a6a2972708458be923b1
ivwvh <68565784+ivwvh@users.noreply.github.com>
2022-09-07 21:42:37 +0300

    Add files via upload

---
+++ ints.py
@@ -0,0 +1,9 @@
+num1=int(input("Введите наименьшее число: "))
+num2=int(input("Введите наибольшее число: "))
+if num2>num1 :
+	(print (num2))
+else:
+    (print(num1))
+if num1==num2:
+        (print("Числа равны"))
+
