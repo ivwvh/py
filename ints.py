b3cc37ae17b9615133e87792ccd515da310fe374
ivwvh <68565784+ivwvh@users.noreply.github.com>
2022-09-07 22:30:49 +0300

    Add files via upload

---
+++ ints.py
@@ -0,0 +1,9 @@
+numA=int(input("Введите первое число: "))
+numB=int(input("Введите второе число: "))
+if numA< numB :
+	(print (numA))
+else:
+    (print(numB))
+if numA==numB:
+        (print("Числа равны"))
+
