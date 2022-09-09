8b0916f7e46f79c756bd2f06616c369639e4bf09
ivwvh <68565784+ivwvh@users.noreply.github.com>
2022-09-09 16:47:19 +0300

    Add files via upload

---
+++ curr.py
@@ -0,0 +1,7 @@
+usermoney=float(input("Сколько у вас денег "))
+dinar=196.56
+rial=0.0014
+usdollar=60.63
+print("У вас",round(usermoney/dinar,2),"кувейтских динаров")
+print("У вас",round(usermoney/rial,2),"иранских риалов")
+print("У вас",round(usermoney/usdollar,2),"долларов")
