e9289c2935a34c5e5dd85fe2a8c261a33d11840b
ivwvh <68565784+ivwvh@users.noreply.github.com>
2022-09-21 12:15:46 +0300

    Update dice2.py

--- dice2.py
+++ dice2.py
@@ -1,24 +1,25 @@
 import random
-coin1 = 10
-coin2 = 10
-while coin1 > 0 or coin2 > 0:
-    pirate1 = random.randint(1,6)
-    pirate2 = random.randint(1,6)
-    if pirate1 > pirate2:
-        coin1 += 1
-        coin2 -= 1
-        print("Выиграл первый пират")
-    elif pirate1 == pirate2:
-        coin1 += 0
-        coin2 += 0
-        print("Ничья")
-    elif pirate1 < pirate2:
-        coin1 -= 1
-        coin2 += 1
-        print("Выиграл второй пират")
-    if coin1 == 0 or coin2 == 0:
+
+coin_1 = 10
+coin_2 = 10
+while coin_1 > 0 or coin_2 > 0:
+    if coin_1 == 0 or coin_2 == 0:
         break
-if coin1 > coin2:
+    pirate_1 = random.randint(1, 6)
+    pirate_2 = random.randint(1, 6)
+    if pirate1 > pirate2:
+        coin_1 += 1
+        coin_2 -= 1
+        print("Выиграл первый пират")
+    elif pirate_1 == pirate_2:
+        coin_1 += 0
+        coin_2 += 0
+        print("Ничья")
+    else:
+        coin_1 -= 1
+        coin_2 += 1
+        print("Выиграл второй пират")
+if coin_1 > coin_2:
         print("Победил первый пират")
 else:
         print("Победил второй пират")
