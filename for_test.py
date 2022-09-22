d7036bbc16cd98f3147341a0aa5b1e62e5247ba5
ivwvh <68565784+ivwvh@users.noreply.github.com>
2022-09-22 15:08:38 +0300

    Update for_test.py

--- for_test.py
+++ for_test.py
@@ -1,6 +1,4 @@
 number_1 = int(input())
 number_2 = int(input())
-for numbers in range(number_1, number_2):
-    if number_1 == number_2:
-        break
+for numbers in range(number_1, number_2 + 1):
     print(numbers)
