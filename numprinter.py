1bef42f94c22c362cb62be8f5ae1da65e2724b9b
ivwvh <68565784+ivwvh@users.noreply.github.com>
2022-09-30 17:43:25 +0300

    Update numprinter.py

--- numprinter.py
+++ numprinter.py
@@ -3,11 +3,11 @@
 start= int(input("Первое число:"))
 stop = int(input("Второе число:"))
 x = 0
-if start %2 == 0 and stop %2 == 0:
+if start % 2 == 0 and stop % 2 == 0:
     for x in range (start, stop + 2 , 2):
         print (x)
         x += 2
 else:
-    for x in range (start, stop , 2 ):
+    for x in range (start +1, stop , 2 ):
         x += 2
         print (x)
